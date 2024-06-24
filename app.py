0ae9d6d4e74c1906a2775a2493fdabb0b65d4bb1
jumaerick <132266012+jumaerick@users.noreply.github.com>
2024-06-24 10:46:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,16 +3,28 @@ from explore import explorer
 from explore import decompose
 
 st.title('Time Series Analysis')
-option = st.sidebar.selectbox('Select a task', ('explore','decompose','forecast'))
+option = st.sidebar.selectbox('Select a task', ('Explore','Decompose','Stationarity Test', 'Forecasting'))
 
 def main(item):
+    item = item.lower()
     if item == 'explore':
         explorer.distributions()
     elif item == 'decompose':
+        # decompose.autoCorrelation()
         st.markdown('Decomposing the time series to its components')
         decompose.seasonalComponentPlot()
+        st.markdown('The time series exhibits seasonality since peaks and troughs occur annualy')
         decompose.residualComponentPlot()
+        st.markdown('There exists unpxplained noise/randomness in the data')
         decompose.trendComponentPlot()
+        st.markdown('The time series is moving in an upwards general direction')
+        st.markdown("")
+        st.markdown('The rolling mean and standard deviation at a window ')
+        decompose.rollingMeanStd()
+
+    elif item == 'stationarity test':
+        # st.markdown('Stationarity Test')
+        decompose.stationarityTest()
     else:
         pass
 
