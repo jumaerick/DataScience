a285f63d73848428494148d18e26e6df8d408a21
jumaerick <132266012+jumaerick@users.noreply.github.com>
2024-07-04 15:48:56 +0300

    Update decompose.py

--- explore/decompose.py
+++ explore/decompose.py
@@ -12,10 +12,10 @@ def autoCorrelation():
     st.pyplot(plt)
 
 
-def seasonalComponentPlot():
-    fig = plt.figure(figsize=(12, 6))
-    pd.plotting.autocorrelation_plot(dataset.iloc[:, :2])
-    st.pyplot(plt)
+# def seasonalComponentPlot():
+#     fig = plt.figure(figsize=(12, 6))
+#     pd.plotting.autocorrelation_plot(dataset.iloc[:, :2])
+#     st.pyplot(plt)
 
 
 def seasonalComponentPlot():
@@ -151,6 +151,7 @@ def rollingMeanStd():
     ts_mastd = dataset.iloc[:, :1] - rolling_std
 
     fig = plt.figure(figsize=(10, 5))
+    plt.plot(dataset.iloc[:, :1])
     plt.plot(dataset.iloc[:, :1], color='blue', label="Original Medical data")
     plt.plot(ts_maverage, color='red', label="Rolling Mean Patient Number")
     plt.plot(ts_mastd, color='black',
@@ -158,19 +159,4 @@ def rollingMeanStd():
     plt.legend(loc='best')
     st.pyplot(plt)
 
-    # if dfoutput['p-value'] <= threshold:
-    #     st.table(dfoutput)
-    #     st.markdown(
-    #         'Since the p-value < %.2f we reject the null hypothesis' % (threshold))
-    #     st.markdown(
-    #         'Therefore the timeseries is now stationary after performing differencing of order %i' % (order))
-    # else:
-    #     st.table(dfoutput)
-    #     if (order == 0):
-    #         st.markdown(
-    #             'Since the p-value > %.2f we accept the null hypothesis' % (threshold))
-    #         st.markdown('Therefore the timeseries is not stationary')
-    #     else:
-    #         st.markdown(
-    #             'Since the p-value > %.2f the differencing of order %i does not make the time series stationary' % (threshold, order))
-    #         st.markdown('Continue with higher order differencing')
+
