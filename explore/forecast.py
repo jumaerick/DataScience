180f2bb3a1f1caca192b09d1dab3a55f855640a1
jumaerick <132266012+jumaerick@users.noreply.github.com>
2024-07-08 09:43:20 +0300

    Update forecast.py

--- explore/forecast.py
+++ explore/forecast.py
@@ -4,7 +4,7 @@ from statsmodels.tsa.stattools import adfuller
 from statsmodels.graphics.tsaplots import plot_acf
 from statsmodels.graphics.tsaplots import plot_pacf, plot_predict
 from statsmodels.tsa.arima.model import ARIMA
-from statsmodels.tsa.arima.model import sarimax
+from statsmodels.tsa.statespace.sarimax import SARIMAX 
 
 dataset = dataLoader.getData()
 results = seasonal_decompose(dataset['value (million $)'], model='additive')
@@ -24,17 +24,21 @@ def autoCorrelation():
     st.markdown('AR(1) since there is a spike at lag 1 and upto lag 12 again which signifies seasonality')
     st.markdown('MA(4) since there is a spike at lag 1 to 4 and again from lag 12 which signifies seasonality')
     st.markdown('Our ARIMA model will have the terms p = 4, d = 1, q=1')
+    st.text("Equation (p,d,q)")
+    AR = "p the lags of dependent variable used"
+    MA = "q are lagged forecast errors in prediction equation"
+    d =  "Number of differencing"
     model = ARIMA(ts_diff, order=(4, 1, 1))  
     results_AR = model.fit()  
-    # st.text(results_AR.summary())
+    print(results_AR.summary())
 # Actual vs Fitted
     forecast_steps = 12  # Number of steps to forecast
-    forecast = results_AR.predict(start=len(ts_diff)-forecast_steps, end=len(ts_diff) + forecast_steps - 1, typ='levels')
+    forecast = results_AR.predict(start=len(ts_diff)-1, end=len(ts_diff) + forecast_steps - 1, typ='levels')
     # Convert forecasted values to a pandas Series with appropriate index
     # forecast_index = pd.date_range(start=ts_diff.index[-1], periods=forecast_steps + 1, freq='M')[1:]
 
     # forecast_series = pd.DataFrame(forecast, index=forecast_index.date)
-    st.text(forecast)
+    # st.text(forecast)
 
     # Print or display the forecasted values
     # print("Forecasted Values:")
@@ -54,6 +58,21 @@ def autoCorrelation():
     st.header('SARIMA')
     st.markdown('The presence of the seasonal component imply that the SARIMA model will be the better choice')
 
+    modelSarimax = SARIMAX(ts_diff, 
+                order=(4, 1, 1),          # non-seasonal part: (p, d, q)
+                seasonal_order=(1, 1, 1, 12))  # seasonal part: (P, D, Q, s)
+    
+    results_SAR = modelSarimax.fit()  
+    print(results_SAR.summary())
+    # st.text(results_SAR)
 
+    forecast = results_SAR.predict(start=len(ts_diff)-1, end=len(ts_diff) + forecast_steps - 1, typ='levels')
 
+    plt.figure(figsize=(12, 6))
+    plt.plot(ts_diff, color='blue', label='Actual')
+    plt.plot(results_SAR.fittedvalues, color='red', label='Fitted')
+    plt.plot(forecast, color='green', label='Forecasted')
+    plt.title('Actual, Fitted, and Forecasted Values')
+    plt.legend(loc='best')
+    st.pyplot(plt)
 # lag_acf = acf(dataset['value (million $)'], nlags=20)
