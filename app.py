7881eddff38fd13ef610e29791708fc56032e252
Erick Juma <jumaaerick@gmail.com>
2024-09-09 10:35:41 +0300

    added-users

--- app.py
+++ app.py
@@ -21,12 +21,33 @@ def main(option):
         if analysisype == 'Bivariant':
             # explorer.genderDistribution() 
             explorer.featureComparison()
+            st.markdown("""
+                - Annual income and spending score plot reveals the following groups:
+                    - Low income  with low spending score
+                    - Low income  with high spending score
+                    - Medium income  with medium spending score
+                    - High income  with low spending score
+                    - High income  with high spending score
+                - Annual income and age plot reveals the following groups:
+                    - In general, the audience under 30 years and those over 50 years have low and average annual income i.e under 80k.
+                    - Audience between 30 and 50 years are spread out from low, medium to high annual income.
+                - Spending score and age plot imply some loose groupings about 3, this is the same for annual income and age plot.
+                
+            """)
+
             explorer.bucketsComparisons()
+            st.markdown("""
+                -  Majority of the audience were below  30yrs of age.
+                -  Females formed the majority of most agebrackets with males comprising of audience in their fifties and above only.    
+                -  In general, were the mojority in all the spending score groups.
+                -  Majority of the audience were under the medium spending score group.
+                -  Majority of the audience fell under the group of medium income earners.    
+                -  High income earners were equally distributed among males and females.               
+                       """)
         else:
             explorer.basicInfo()
     elif option == 'Prediction':
-        cluster = predictor.Clustering()
-        st.text(cluster.test())
+        predictor.performClustering()
     else:
         st.markdown("""
                     In this project, we are going to explore the mall customers dataset. The dataset consist various 
@@ -36,7 +57,7 @@ def main(option):
                 - Segment the customers
                 - Build an ML algorithm and predict the cluster where the customer belongs
                 - Analyse the Davies_Bouldin and Silhouette_score as metrics for choosing k values
-                       
+                    
                     """)
     pass
 
--- predict/predictor.py
+++ predict/predictor.py
@@ -3,13 +3,41 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
+from sklearn.cluster import KMeans
+from sklearn.metrics import davies_bouldin_score, silhouette_score
 
 dataset = dataLoader.loadData()
 
 
 class Clustering:
     def __init__(self, n_iters = 10):
+        self.dataset = dataset
         self.n_iters = n_iters
 
     def test(self):
         return self.n_iters
+    
+    def scoring(self, features):
+        """
+        Define the model
+        """
+        X = self.dataset[features]
+        scores = {'iter': [], 'inertia': [], 'dbs': [], 'shs': []}
+        for i in range(2, self.n_iters):
+            model = KMeans(n_clusters = i)
+            model.fit(self.dataset[features])
+            clusters = model.labels_
+            scores['iter'].append(i)
+            scores['inertia'].append(model.inertia_)
+            scores['dbs'].append(davies_bouldin_score(X, labels = clusters))
+            scores['shs'].append(silhouette_score(X, labels = clusters))
+        fig = plt.figure()
+        plt.plot( scores['iter'], scores['shs'])
+        plt.plot( scores['iter'], scores['dbs'])
+        return st.pyplot(plt)
+        
+    
+cluster = Clustering()
+
+def performClustering():
+    return cluster.scoring(['Age','Spending Score (1-100)'])
