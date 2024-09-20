b442ca6fd047737bc2850aa15334aebb2c53e931
Erick Juma <jumaaerick@gmail.com>
2024-09-20 10:24:03 +0300

    new

--- explore/dataLoader.py
+++ explore/dataLoader.py
@@ -8,17 +8,17 @@ Read the file to a list of dictionaries
 """
 path = '.'
 def loadData(filename = 'Mall_customers.csv'):
-    file = os.path.join(path, filename)
-    with open(file) as openFile:
-        lines = [line.strip().split(',') for line in openFile.readlines()]
-        cols =  lines[0]
-        data = [{cols[key]:val for key, val in enumerate(line)} for line in lines[1:]]
-    df = pd.DataFrame(data)
-    # df.columns.tolist().remove(df.columns[df.columns.tolist().index('Gender')])
-    cols =   df.columns.tolist()
-    cols.remove('Gender')
-    for col in cols:
-        df[col] = df[col].astype(float)
+    # file = os.path.join(path, filename)
+    # with open(file) as openFile:
+    #     lines = [line.strip().split(',') for line in openFile.readlines()]
+    #     cols =  lines[0]
+    #     data = [{cols[key]:val for key, val in enumerate(line)} for line in lines[1:]]
+    # df = pd.DataFrame(data)
+    # # df.columns.tolist().remove(df.columns[df.columns.tolist().index('Gender')])
+    # cols =   df.columns.tolist()
+    # cols.remove('Gender')
+    # for col in cols:
+    #     df[col] = df[col].astype(float)
     # df = [df[col].astype(float) for col in cols]
     # st.text(df)
     df = pd.read_csv('Mall_Customers.csv')
