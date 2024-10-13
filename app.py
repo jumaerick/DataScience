ad6fcf90d4aa026cb4fcf78e14cd362d97bff670
Juma Erick <jumaaerick@gmail.com>
2024-10-13 17:41:07 +0300

    changes:

--- app.py
+++ app.py
@@ -1,13 +1,46 @@
 import streamlit as st
-from modules import fileReader
+from modules import fileReader, calculator
 
-st.title('heeloo')
 
-dataset = fileReader.importData()
+st.title('Target Investment Calculator')
+st.markdown(
+    """
+    <style>
+    .stNumberInput div[data-baseweb="input"] > div:first-child {
+        background-color: mediumspringgreen;
+    }
+    
+        .stSlider div[data-baseweb="slider"] > div:first-child {
+        color: lightseagreen;
+    }
+    tr:nth-child(even){
+        background-color:#f2f2f2;
+        }
+        
+            th:nth-child(1){
+       display:none;
+        }
+        
+    th{
+              background-color:#f2f2f2; 
+    }
+    
+    .col_heading{
+        color:#000;
+    }
+
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+# dataset = fileReader.importData()
 
 def main():
-    fileReader.sampleStats()
-    fileReader.sampleChart()
+    # fileReader.sampleStats()
+    # fileReader.sampleChart()
+    calculator.calculate()
+    # st.text(calculator.recursive())
+    # st.text(calculator.recurse())
 
 if __name__=='__main__':
     main()
