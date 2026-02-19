1a1a2030796cc9db036b6b16436d536d1b4f4e16
jumaa_erick <erick@farwell-consultants.com>
2026-02-19 12:40:52 +0300

    added-folder

--- playScrapper.py
+++ playScrapper.py
@@ -1,5 +1,6 @@
 from playwright.sync_api import sync_playwright
 import pandas as pd
+from datetime import datetime
 
 # Create an SSL context with certifi's CA bundle
 
@@ -7,7 +8,7 @@ url = 'https://erevuka.org/courses'
 
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=False)
-    context = browser.new_context() 
+    context = browser.new_context()
     page = context.new_page()
     
     print("Loading courses page...")
@@ -17,44 +18,72 @@ with sync_playwright() as p:
 
     pagination = page.locator('.pagination').first
 
-    cards = pagination.locator('.page-item')
+    pages = pagination.locator('.page-item')
     links = {}
-
+    start = datetime.now()
     """card one is disabled and last leads nowhere so we exclude"""
-    for i in range(1, cards.count()-1, 1):
-        card = cards.nth(i)
+    for i in range(1, pages.count()-1, 1):
+        pager = pages.nth(i)
         newUrl = url+f'?page={i}'
-        card.click()
+        pager.click()
         """click each page to view courses {i}"""
         print(f'page {i} clicked')
-        page.wait_for_selector('.row.alignment-class.mt-3', timeout = 150000)
+        page.wait_for_selector('.row.alignment-class.mt-3', timeout=150000)
 
         newRow = page.locator('.row.alignment-class.mt-3').first
         # print(newRow)
 
         courseCards = newRow.locator('.course-card-content')
 
-        print('Number of course', courseCards.count())
+        cardFooters = newRow.locator('.card-footer')
 
-        # page.wait_for_selector('.courses-detail-row', timeout = 180000)
+        print(f'Number of courses on page {i}', courseCards.count())
 
         for i in range(courseCards.count()):
             href = courseCards.nth(i).locator('a').get_attribute('href')
             title = courseCards.nth(i).locator('a').get_attribute('title')
-            links.update({title: {'link': href}})
-        page.goto(newUrl)
-    
-    # Open in new page manually and close once done with scrapping
-    for name in links:
-        print(name)
-        new_page = page.context.new_page()
-        new_page.goto(links[name]['link'], wait_until="networkidle" )
-        print(f'Going to page {name}')
+            """fetching the second child of the footer"""
+            action = cardFooters.nth(i).locator('a')
+            lastAction = action.last
+            """We check if the action text is all modules extract inner modules links"""
+            if (lastAction.text_content() == 'All Modules'):
+                innerPage = page.context.new_page()
+                innerPage.goto(lastAction.get_attribute(
+                    'href'), wait_until='domcontentloaded')
+                print(f'Opening inner pages for {title}')
+                innerPage.wait_for_selector(
+                    '.row.alignment-class.mt-3', timeout=150000)
+                innerRow = innerPage.locator('.row.alignment-class.mt-3').last
+                """There are rows in the inner page so we pick the last one"""
+                innerCards = innerRow.locator('.course-card-content')
+                print('Extracting links for inner pages')
+                for j in range(innerCards.count()):
+                    innerHref = innerCards.nth(j).locator(
+                        'a').get_attribute('href')
+                    innerTitle = innerCards.nth(j).locator('a').inner_text()
+                    links.update({innerTitle: {'link': innerHref}})
+                innerPage.close()
+            else:
+                links.update({title: {'link': href}})
 
+        page.goto(newUrl)
+
+    print(f'Done extracting links. Total links extracted: {len(links)}')
+    print('Scrapping of each page')
+    """Open in new page manually and close once done with scrapping"""
+    for name in links:
+        new_page = page.context.new_page()
+        new_page.goto(links[name]['link'], wait_until="networkidle")
+        # print(f'Extracting description and outlines of {name} course')
         try:
-            new_page.wait_for_selector('.row.courses-detail-row', timeout=10000)
-            """Get all li of ul"""
-            print('element visible')
+            new_page.wait_for_selector(
+                '.row.courses-detail-row', timeout=10000)
+            # print('Details row visible proceed')
+
+            """Get all the tabs on the details page we will exclude testimonials.
+            Since the tabs reload the dom we extract their hrefs for each course
+            """
+
             listItems = new_page.locator('.nav.nav-pills.course-tabs li')
 
             tabRefs = {}
@@ -63,88 +92,48 @@ with sync_playwright() as p:
                 first = listItems.nth(k).first
                 tabTitle = first.text_content()
                 tabHref = first.locator('a').get_attribute('href')
-                tabRefs.update({tabTitle: links[name]['link']+tabHref })
+                tabRefs.update({tabTitle: links[name]['link']+tabHref})
                 # print(f'clicked onn {tabTitle}')
-            
-            print(tabRefs)
-
+            """For each tab click and wait to load to extract content before proceeding.
+            The clicks are fast so we have reload the page for second tab click.
+            """
             for ref in tabRefs:
-                print(ref)
                 new_page.goto(tabRefs[ref], wait_until='domcontentloaded')
                 tabers = new_page.locator('ul.course-tabs a.nav-link')
-                if(ref=='Description'):
+                if (ref == 'Description'):
                     taber = tabers.nth(0)
                     taber.click()
-                    new_page.reload(wait_until='domcontentloaded')
-                    new_page.wait_for_selector('.row-description',  timeout=20000)
-                    child = new_page.locator('.row-description')
+                    new_page.wait_for_selector(
+                        '.row-description',  timeout=20000)
+                    child = new_page.locator('.row-description p')
                     description = child.nth(0).inner_text()
-                elif(ref =='Course Outline'):
+                elif (ref == 'Course Outline'):
                     taber = tabers.nth(1)
                     taber.click()
                     new_page.reload(wait_until='domcontentloaded')
-                    new_page.wait_for_selector('.row-description',  timeout=20000)
+                    new_page.wait_for_selector(
+                        '.row-description',  timeout=20000)
                     child = new_page.locator('.row-description')
                     lister = child.nth(1)
-                    all_texts = [li.text_content() for li in lister.locator('ul li').all()]
-                    # print(all_texts, 'all textx')
+                    """extract li items to comma separated string"""
+                    all_texts = [li.text_content()
+                                for li in lister.locator('ul li').all()]
                     comma_separated = ', '.join([t.strip() for t in all_texts])
 
                     description = comma_separated
 
-                links[name].update({ref : description})
-                    # print(links)
-
-                # print(ref, description)
-            
-            
-
-            # for k in range(0, listItems.count()-1, 1):
-            #     first = listItems.nth(k).first
-            #     tabTitle = first.text_content()
-            #     print(f'clicked onn {tabTitle}')
-
-                # first.click()
-                # new_page.wait_for_selector('.row-description',  timeout=10000)
-                # description = new_page.locator('.row-description')
-                # print(description.inner_text())
-
-            # for k in range(listItems.count()):
-            #     tabTitle = listItems.nth(k).text_content()
-            #     pillhref = listItems.nth(k).locator('a').get_attribute('href')
-
-            #     tabs_page = page.context.new_page()
-            #     tabs_page.goto(links[name]['link'] + pillhref)
-
-            #     print(f'Going to page {name} +  {tabTitle}')
-            #     tabs_page.wait_for_selector('.row-description',  timeout=10000)
-            #     description = tabs_page.locator('.row-description')
-            #     links[name].update({tabTitle : description.text_content()})
-            #     print(description.text_content())
-            #     tabs_page.goto(links[name]['link'])
-            #     # listItems.nth(k).click(no_wait_after=True)
-            #     # new_page.wait_for_selector(f'.tab-pane.active')  # Wait for tab content
-            #     new_page.wait_for_timeout(300)
-            #     new_page.wait_for_selector('.row-description',  timeout=10000)
-            #     description = new_page.locator('.row-description')
-            #     links[name].update({tabTitle : description.text_content()})
-            #     print(links[name])
+                links[name].update({ref: description})
+            # print(f'Course description and outline updated.')
 
         except:
-            print('selector not found')
+            print('Course details selector not found')
 
         new_page.close()
-
-    df = pd.DataFrame.from_dict(links, orient="index").reset_index().rename(columns={'index':'Course Name'})
-    df.to_csv('courses-result.csv', index=False)
-    # for pager in range(pagination.first.count()):
-    #     card = pagination.nth(pager)
-    #     print(card)
-        # card.locator('.course-card-content')
-        # card.click()
-
-
-
-    # print(page)
-
-    
+    df = pd.DataFrame.from_dict(links, orient="index").reset_index().rename(
+        columns={'index': 'Course Name'})
+    df.to_csv('./datasets/courses-result.csv', index=False)
+    records = df.shape[0]
+    end = datetime.now()    
+    print('Task ended on ', end)
+    print('process took', end - start)
+    print(f'Task Completed. {records} Record Saved!!')
